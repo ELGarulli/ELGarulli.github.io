d925905b8406ffbb33f0f76688f68c9f83d88def
ELGarulli <elisa.garulli@charite.de>
2026-03-31 13:57:42 +0200

    first attempts

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const cards = document.querySelectorAll(".card");
 
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
+  });
+});
+
+cards.forEach(card => observer.observe(card));
